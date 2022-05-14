8c93fe204693191d8dbc09711829e90d517b562e
choihocheol <show1693@gmail.com>
2022-05-14 22:07:10 +0900

    feat: Add permutation function

--- getPermutation.js
+++ getPermutation.js
@@ -3,10 +3,10 @@ const getPermutation = function (arr, r) {
     if (r === 1) return arr.map((e) => [e]);
 
     arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index+1)];
-      const permutation = getPermutation(rest, r - 1);
-      const attached = permutation.map((e) => [fixed, ...e]);
-      result.push(...attached);
+        const rest = [...origin.slice(0, index), ...origin.slice(index+1)];
+        const permutation = getPermutation(rest, r - 1);
+        const attached = permutation.map((e) => [fixed, ...e]);
+        result.push(...attached);
     });
 
     return result;
