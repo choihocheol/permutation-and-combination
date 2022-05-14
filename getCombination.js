34901dde52f21e4cf3ea4c3abeaaf491cb9c15aa
choihocheol <show1693@gmail.com>
2022-05-14 22:06:55 +0900

    feat: Add combination function

--- getCombination.js
+++ getCombination.js
@@ -1,12 +1,12 @@
- const getCombination = function (arr, r) {
+const getCombination = function (arr, r) {
     const result = [];
     if (r === 1) return arr.map((e) => [e]);
 
     arr.forEach((fixed, index, origin) => {
-      const rest = origin.slice(index + 1);
-      const combination = getCombination(rest, r-1);
-      const attached = combination.map((e) => [fixed, ...e]);
-      result.push(...attached);
+        const rest = origin.slice(index + 1);
+        const combination = getCombination(rest, r-1);
+        const attached = combination.map((e) => [fixed, ...e]);
+        result.push(...attached);
     });
 
     return result;
